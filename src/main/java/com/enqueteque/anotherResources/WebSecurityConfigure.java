0bf657e7340ed9a92eb0282f19600d0dcb7369a6
Diego <diegocdsantos@gmail.com>
2023-09-14 16:53:14 -0300

    configured the security to require authentication to the post method for create new polls, now i have to implement the authentication method

--- src/main/java/com/enqueteque/anotherResources/WebSecurityConfigure.java
+++ src/main/java/com/enqueteque/anotherResources/WebSecurityConfigure.java
@@ -1,33 +1,32 @@
-//package com.enqueteque.anotherResources;
-//
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-//import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-//import org.springframework.security.crypto.password.PasswordEncoder;
-//import org.springframework.security.web.SecurityFilterChain;
-//import org.springframework.security.config.Customizer;
-//
-//
-//@Configuration
-//@EnableWebSecurity
-//public class WebSecurityConfigure {
-//
-//    @Bean
-//    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-//        System.out.println("Passou por aqui");
-//        http.
-//                authorizeHttpRequests((requests)->requests
-//                .requestMatchers("/api/v1/poll/newPoll").authenticated()
-//                .anyRequest().permitAll())
-//                .formLogin(Customizer.withDefaults())
-//                .httpBasic(Customizer.withDefaults());
-//        return http.build();
-//    }
-//
-//    @Bean
-//    public PasswordEncoder getPasswordEncoder(){
-//        return new BCryptPasswordEncoder();
-//    }
-//}
+package com.enqueteque.anotherResources;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfigure {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.csrf(AbstractHttpConfigurer::disable).
+            authorizeHttpRequests(authCustomizer -> authCustomizer
+                .requestMatchers(HttpMethod.POST, "/api/v1/vote").permitAll()
+                    .requestMatchers(HttpMethod.POST , "/api/v1/poll").denyAll()
+                    .anyRequest().permitAll());
+        return http.build();
+    }
+
+    @Bean
+    public PasswordEncoder getPasswordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+}
